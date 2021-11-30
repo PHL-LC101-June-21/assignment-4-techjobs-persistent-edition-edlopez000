4ab1df3a7f57ad8ec70f9d4083681e2b9a111b7e
edlopez000 <lopez.edward@outlook.com>
2021-11-29 19:41:31 -0500

    Finished Employer and Skills models and data layers

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull(message = "Location is required!")
+    @Size(min = 5, max = 20)
+    private String location;
 
+    public Employer() {};
+
+    public String getLocation() {
+        return location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,17 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 140)
+    private String description;
+
+    public Skill() {};
+
+    public String getDescription() {
+        return description;
+    }
 }
