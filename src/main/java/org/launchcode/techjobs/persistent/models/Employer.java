6c02d3eb8082d0ed9620a579a21611fa05c552a5
edlopez000 <lopez.edward@outlook.com>
2021-12-02 23:23:14 -0500

    Added a jobs field to Employer

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +14,10 @@ public class Employer extends AbstractEntity {
     @NotNull(message = "Location is required!")
     @Size(min = 5, max = 20)
     private String location;
+
+    @OneToMany(mappedBy = "id")
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer() {};
 
